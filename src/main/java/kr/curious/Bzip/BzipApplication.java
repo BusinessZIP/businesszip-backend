a4d3ccdbe2938631cf87346a3ab68ede34771e3b
npole0103 <npole0103@naver.com>
2022-06-22 20:23:25 +0900

    Initialize Spring Boot

---
+++ src/main/java/kr/curious/Bzip/BzipApplication.java
@@ -0,0 +1,13 @@
+package kr.curious.Bzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/curious/Bzip/BzipApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.curious.Bzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
