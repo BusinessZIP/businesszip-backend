62b0677dcf0e7eb6657f8ee14224d65fc1e1ec62
SonMinWoo <mwson98@naver.com>
2022-06-23 17:09:51 +0900

    feat: Add signup

--- src/main/java/kr/curious/Bzip/controller/MemberController.java
+++ src/main/java/kr/curious/Bzip/controller/MemberController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,21 +39,30 @@ public class MemberController {
         return jsonObject.toString();
     }
 
-//    @ResponseBody
-//    @PostMapping("/signup")
-//    public String Signup(@RequestParam String email, @RequestParam String password) throws Exception {
-//
-//        JsonObject jsonObject = new JsonObject();
-//        jsonObject.addProperty("code", StatusCode.UNAUTHORIZED);
-//
-//        Optional<Member> memberOptional = memberRepository.findByEmailAndPassword(userId, password);
-//
-//        memberOptional.ifPresent(member -> {
-//            jsonObject.addProperty("code", StatusCode.OK);
-//            jsonObject.addProperty("token", jwtUtil.createToken(member.getId().toString()));
-//        });
-//        return jsonObject.toString();
-//    }
+    @ResponseBody
+    @PostMapping("/signup")
+    public String Signup(@RequestParam String email, @RequestParam String password, @RequestParam String name, @RequestParam String phoneNumber)
+            throws Exception {
+        JsonObject jsonObject = new JsonObject();
+
+
+        Optional<Member> memberOptional = memberRepository.findByEmail(email);
+        if(!memberOptional.isPresent()) {
+            Member member = new Member();
+            member.setEmail(email);
+            member.setPassword(password);
+            member.setName(name);
+            member.setPhoneNumber(phoneNumber);
+            member.setStatus("REGISTERED");
+            member.setCreatedAt(LocalDateTime.now());
+            memberRepository.save(member);
+            jsonObject.addProperty("code", StatusCode.CREATED);
+        } else {
+            jsonObject.addProperty("code", StatusCode.UNAUTHORIZED);
+        }
+
+        return jsonObject.toString();
+    }
 
     @ResponseBody
     @PostMapping("/test")
